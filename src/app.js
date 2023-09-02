9b25d91594c9610aef0e04aded29462a5c02fa06
AntonioDelGreco <antoniodelgreco@icloud.com>
2023-09-02 09:28:26 -0300

    arreglos al cors 3

--- src/app.js
+++ src/app.js
@@ -6,17 +6,17 @@ const turnosRouter = require('./routes/turnos.router.js');
 
 dotenv.config()
 const app = express();
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
-
 const corsOptions = {
   origin: process.env.DOMINIO_FRONT,
   methods: 'POST',
   credentials: true,
 };
-
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(cors(corsOptions));
+
 app.use('/', turnosRouter);
 app.use('/', contactoRouter);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
