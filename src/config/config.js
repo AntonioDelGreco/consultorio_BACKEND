51ff397ebefcf15462966291ff58fd4d7d13c6ad
AntonioDelGreco <antoniodelgreco@icloud.com>
2023-09-02 09:08:33 -0300

    Arreglos al cors

--- src/config/config.js
+++ src/config/config.js
@@ -1,15 +1,17 @@
-const dotenv = require('dotenv')
-dotenv.config();
+require('dotenv').config();
 
 const corsOptions = {
-  origin: function(origin, callback){
-    if (process.env.DOMINIO_FRONT.indexOf(origin) !== -1) {
-      callback(null, true);
-    }
-    else{
-      callback(new Error('No permitido por CORS'));
-    }
-  }
+  origin:[process.env.DOMINIO_FRONT],
+  methods:"POST",
+  credentials: true
+  // origin: function(origin, callback){
+  //   if (process.env.DOMINIO_FRONT.indexOf(origin) !== -1) {
+  //     callback(null, true);
+  //   }
+  //   else{
+  //     callback(new Error('No permitido por CORS'));
+  //   }
+  // }
 }
 
 module.exports = corsOptions;
